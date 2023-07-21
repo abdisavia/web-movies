8efbf93661312857f70bd5ee2933da1bf8daeee9
abdisavia <ridhodanabdi7a@gmail.com>
2023-07-21 17:57:30 +0700

    NEW UPDATE, Pagination Worked

--- Website Clone IMDB/FrontEnd/js/script.js
+++ Website Clone IMDB/FrontEnd/js/script.js
@@ -1,11 +1,48 @@
-$.ajax({
-    movies:[{}],
-    url: "http://www.omdbapi.com/?apikey=929c59ff&t=transformer",
-    success: (movies) =>{
-        console.log(movies)
-        return movies
-    },
-    error: () => {
-        alert("Movie not found")
+let ulImg_jumb = document.querySelector("#imgJumb")
+let listImg_jumb = ulImg_jumb.querySelectorAll("li")
+let ulPagination_jumb = document.querySelector("#pagination")
+let pagination = ulPagination_jumb.querySelectorAll("li")
+console.log(listImg_jumb.length != 0)
+console.log(pagination)
+
+function changeImg(e){
+    listImg_jumb.forEach(img=>{
+        if(!img.classList.contains("hidden")){
+            img.classList.add("hidden")
+        }
+    })
+    listImg_jumb[e].classList.remove("hidden")
+}
+
+function addPagination(){
+    if(listImg_jumb.length != 0){
+        let index = -1
+        listImg_jumb.forEach(()=>{
+            ulPagination_jumb.appendChild(createBtnPagination(index+=1))
+        })
     }
-});
+}
+
+function createBtnPagination(index){
+    let paginat = document.createElement("li")
+    let btnPagination = document.createElement("button")
+    paginat.appendChild(btnPagination)
+    btnPagination.setAttribute('class',"bg-white rounded-full w-3 h-3  mx-1 hover:bg-gray-300 focus:bg-gray-400")
+    btnPagination.setAttribute('type','button')
+    btnPagination.setAttribute('onclick', 'changeImg('+index+')')
+    return paginat
+}
+addPagination();
+
+
+// $.ajax({
+//     movies:[{}],
+//     url: "http://www.omdbapi.com/?apikey=929c59ff&t=transformer",
+//     success: (movies) =>{
+//         console.log(movies)
+//         return movies
+//     },
+//     error: () => {
+//         alert("Movie not found")
+//     }
+// });
